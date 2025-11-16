1930e12ba154a457c4a8505e4a9a652140eaacee
Shahmeer <shahmeer.hameed@hazelsoft.org>
2025-11-16 16:36:21 +0500

    added a user service

---
+++ Training/Services/UserService/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Training.Models;
+using Training.Dtos.User;
+
+namespace Training.Services.UserService
+{
+    public interface IUserService
+    {
+        Task<ServiceResponse<GetUserDto>> AddUser(AddUserDto user);
+        Task<ServiceResponse<string>> DeleteUser(int id);
+        Task<ServiceResponse<GetUserDto>> UpdateUser(UpdateUserDto updatedUser);
+        Task<ServiceResponse<GetUserDto>> GetUserById(int id);
+    }
+}
---
+++ Training/Services/UserService/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Training.Dtos.User;
+using Training.Models;
+
+namespace Training.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        public Task<ServiceResponse<GetUserDto>> AddUser(AddUserDto user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<string>> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<GetUserDto>> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<GetUserDto>> UpdateUser(UpdateUserDto updatedUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
