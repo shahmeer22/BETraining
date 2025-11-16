5850cc13f407656d5f71e25d1ab97fef3932c8f5
Shahmeer <shahmeer.hameed@hazelsoft.org>
2025-11-16 16:33:24 +0500

    added a user model

---
+++ Training/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Training.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
