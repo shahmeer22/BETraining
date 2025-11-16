338c4d9e66862bcef0db359abfdbb6efcf88773a
Shahmeer <shahmeer.hameed@hazelsoft.org>
2025-11-16 16:37:02 +0500

    added dtos

---
+++ Training/Dtos/User/AddUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Training.Dtos.User
+{
+    public class AddUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
---
+++ Training/Dtos/User/UpdateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Training.Dtos.User
+{
+    public class UpdateUserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
